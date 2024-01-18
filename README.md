8e3c1ffab209e8c1ba06bf891f3cb37c30737626
Russell Smith (Valorem Consulting) <v-rusmit@microsoft.com>
2024-01-18 16:38:56 -0600

    Default Settings

---
+++ README.md
@@ -0,0 +1,84 @@
+# dbus-shelly-plug-accharger
+Integrate Shelly Plug into Victron Energies Venus OS
+
+## Purpose
+With the scripts in this repo it should be easy possible to install, uninstall, restart a service that connects the Shelly Plug to the VenusOS and GX devices from Victron.
+Idea is inspired on @vikt0rm project linked below.
+
+## Inspiration
+This project with the Victron Venus OS, so I took some ideas and approaches from the following projects - many thanks for sharing the knowledge:
+- https://github.com/vikt0rm/dbus-shelly-1pm-pvinverter
+
+## How it works
+### My setup
+- 1-Phase installation
+- Shelly Plug with latest firmware
+  - Measuring AC output of AC Charger
+  - Connected to Wifi netowrk "A" with a known IP  
+- Venus OS on Raspberry PI 3 4GB
+  - Victron 150/45 Solar Charge Controller
+  - Victron SmartShunt
+  - Connected to Wifi netowrk "A"
+
+### Details / Process
+As mentioned above the script is inspired by @vikt0rm dbus-shelly-1em-pvinverter implementation.
+So what is the script doing:
+- Running as a service
+- connecting to DBus of the Venus OS `com.victronenergy.charger.http_{DeviceInstanceID_from_config}`
+- After successful DBus connection Shelly Plug is accessed via REST-API - simply the /status is called and a JSON is returned with all details
+  A sample JSON file from Shelly 1PM can be found [here](docs/shelly1pm-status-sample.json)
+- Serial/MAC is taken from the response as device serial
+- Paths are added to the DBus with default value 0 - including some settings like name, etc
+- After that a "loop" is started which pulls Shelly Plug data every 750ms from the REST-API and updates the values in the DBus
+
+Thats it 😄
+
+### Pictures
+![Tile Overview](img/venus-os-tile-overview.PNG)
+![Remote Console - Overview](img/venus-os-remote-console-overview.PNG) 
+![SmartMeter - Values](img/venus-os-shelly1pm-pvinverter.PNG)
+![SmartMeter - Device Details](img/venus-os-shelly1pm-pvinverter-devicedetails.PNG)
+
+
+## Install & Configuration
+### Get the code
+Just grap a copy of the main branch and copy them to a folder under `/data/` e.g. `/data/dbus-shelly-plug-accharger`.
+After that call the install.sh script.
+
+The following script should do everything for you:
+```
+wget https://github.com/rsmith0906/dbus-shelly-plug-accharger/archive/refs/heads/main.zip
+unzip main.zip "dbus-shelly-plug-accharger-main/*" -d /data
+mv /data/dbus-shelly-plug-accharger-main /data/dbus-shelly-plug-accharger
+chmod a+x /data/dbus-shelly-plug-accharger/install.sh
+/data/dbus-shelly-plug-accharger/install.sh
+rm main.zip
+```
+⚠️ Check configuration after that - because service is already installed an running and with wrong connection data (host, username, pwd) you will spam the log-file
+
+### Change config.ini
+Within the project there is a file `/data/dbus-shelly-plug-accharger/config.ini` - just change the values - most important is the deviceinstance, custom name and phase under "DEFAULT" and host, username and password in section "ONPREMISE". More details below:
+
+| Section  | Config vlaue | Explanation |
+| ------------- | ------------- | ------------- |
+| DEFAULT  | AccessType | Fixed value 'OnPremise' |
+| DEFAULT  | SignOfLifeLog  | Time in minutes how often a status is added to the log-file `current.log` with log-level INFO |
+| DEFAULT  | Deviceinstance | Unique ID identifying the shelly 1pm in Venus OS |
+| DEFAULT  | CustomName | Name shown in Remote Console (e.g. name of pv inverter) |
+| DEFAULT  | Phase | Valid values L1, L2 or L3: represents the phase where pv inverter is feeding in |
+| DEFAULT  | Position | Valid values 0, 1 or 2: represents where the inverter is connected (0=AC input 1; 1=AC output; 2=AC input 2) |
+| ONPREMISE  | Host | IP or hostname of on-premise Shelly 3EM web-interface |
+| ONPREMISE  | Username | Username for htaccess login - leave blank if no username/password required |
+| ONPREMISE  | Password | Password for htaccess login - leave blank if no username/password required |
+
+
+
+## Used documentation
+- https://github.com/victronenergy/venus/wiki/dbus#pv-inverters   DBus paths for Victron namespace
+- https://github.com/victronenergy/venus/wiki/dbus-api   DBus API from Victron
+- https://www.victronenergy.com/live/ccgx:root_access   How to get root access on GX device/Venus OS
+- https://shelly-api-docs.shelly.cloud/gen1/#shelly1-shelly1pm Shelly API documentation
+
+## Discussions on the web
+This module/repository has been posted on the following threads:
+- https://community.victronenergy.com/questions/127339/shelly-1pm-as-pv-inverter-in-venusos.html
---
+++ dbus-shelly-plug-accharger.py
@@ -0,0 +1,243 @@
+#!/usr/bin/env python
+
+# import normal packages
+import platform
+import logging
+import sys
+import os
+import sys
+if sys.version_info.major == 2:
+    import gobject
+else:
+    from gi.repository import GLib as gobject
+import sys
+import time
+import requests # for http GET
+import configparser # for config/ini file
+
+# our own packages from victron
+sys.path.insert(1, os.path.join(os.path.dirname(__file__), '/opt/victronenergy/dbus-systemcalc-py/ext/velib_python'))
+from vedbus import VeDbusService
+
+
+class DbusShellyService:
+  def __init__(self, servicename, paths, productname='Shelly Plug AC Charger', connection='Shelly Plug AC Charger HTTP JSON service'):
+    config = self._getConfig()
+    deviceinstance = int(config['DEFAULT']['Deviceinstance'])
+    customname = config['DEFAULT']['CustomName']
+
+    self._dbusservice = VeDbusService("{}.http_{:02d}".format(servicename, deviceinstance))
+    self._paths = paths
+
+    logging.debug("%s /DeviceInstance = %d" % (servicename, deviceinstance))
+
+    # Create the management objects, as specified in the ccgx dbus-api document
+    self._dbusservice.add_path('/Mgmt/ProcessName', __file__)
+    self._dbusservice.add_path('/Mgmt/ProcessVersion', 'Unkown version, and running on Python ' + platform.python_version())
+    self._dbusservice.add_path('/Mgmt/Connection', connection)
+
+    # Create the mandatory objects
+    self._dbusservice.add_path('/DeviceInstance', deviceinstance)
+    #self._dbusservice.add_path('/ProductId', 16) # value used in ac_sensor_bridge.cpp of dbus-cgwacs
+    self._dbusservice.add_path('/ProductId', 0xFFFF) # id assigned by Victron Support from SDM630v2.py
+    self._dbusservice.add_path('/ProductName', productname)
+    self._dbusservice.add_path('/CustomName', customname)
+    self._dbusservice.add_path('/Connected', 1)
+
+    self._dbusservice.add_path('/Latency', None)
+    self._dbusservice.add_path('/FirmwareVersion', self._getShellyFWVersion())
+    self._dbusservice.add_path('/HardwareVersion', 0)
+    self._dbusservice.add_path('/Position', int(config['DEFAULT']['Position']))
+    self._dbusservice.add_path('/Serial', self._getShellySerial())
+    self._dbusservice.add_path('/UpdateIndex', 0)
+    #self._dbusservice.add_path('/State', 0)  # Dummy path so VRM detects us as a AC Charger.
+
+    # add path values to dbus
+    for path, settings in self._paths.items():
+      self._dbusservice.add_path(
+        path, settings['initial'], gettextcallback=settings['textformat'], writeable=True, onchangecallback=self._handlechangedvalue)
+
+    # last update
+    self._lastUpdate = 0
+
+    # add _update function 'timer'
+    gobject.timeout_add(250, self._update) # pause 250ms before the next request
+
+    # add _signOfLife 'timer' to get feedback in log every 5minutes
+    gobject.timeout_add(self._getSignOfLifeInterval()*60*1000, self._signOfLife)
+
+  def _getShellySerial(self):
+    meter_data = self._getShellyData()
+
+    if not meter_data['mac']:
+        raise ValueError("Response does not contain 'mac' attribute")
+
+    serial = meter_data['mac']
+    return serial
+
+  def _getShellyFWVersion(self):
+    meter_data = self._getShellyData()
+
+    if not meter_data['update']['old_version']:
+        raise ValueError("Response does not contain 'update/old_version' attribute")
+
+    ver = meter_data['update']['old_version']
+    return ver
+
+  def _getConfig(self):
+    config = configparser.ConfigParser()
+    config.read("%s/config.ini" % (os.path.dirname(os.path.realpath(__file__))))
+    return config;
+
+
+  def _getSignOfLifeInterval(self):
+    config = self._getConfig()
+    value = config['DEFAULT']['SignOfLifeLog']
+
+    if not value:
+        value = 0
+
+    return int(value)
+
+
+  def _getShellyStatusUrl(self):
+    config = self._getConfig()
+    accessType = config['DEFAULT']['AccessType']
+
+    if accessType == 'OnPremise': 
+        URL = "http://%s:%s@%s/status" % (config['ONPREMISE']['Username'], config['ONPREMISE']['Password'], config['ONPREMISE']['Host'])
+        URL = URL.replace(":@", "")
+    else:
+        raise ValueError("AccessType %s is not supported" % (config['DEFAULT']['AccessType']))
+
+    return URL
+
+
+  def _getShellyData(self):
+    URL = self._getShellyStatusUrl()
+    meter_r = requests.get(url = URL)
+
+    # check for response
+    if not meter_r:
+        raise ConnectionError("No response from Shelly Plug AC Charger - %s" % (URL))
+
+    meter_data = meter_r.json()
+
+    # check for Json
+    if not meter_data:
+        raise ValueError("Converting response to JSON failed")
+
+
+    return meter_data
+
+
+  def _signOfLife(self):
+    logging.info("--- Start: sign of life ---")
+    logging.info("Last _update() call: %s" % (self._lastUpdate))
+    logging.info("Last '/Ac/In/L1/V': %s" % (self._dbusservice['/Ac/In/L1/V']))
+    logging.info("--- End: sign of life ---")
+    return True
+
+  def _update(self):
+    try:
+       #get data from Shelly 1pm
+       meter_data = self._getShellyData()
+
+       config = self._getConfig()
+       str(config['DEFAULT']['Phase'])
+
+       accharger_phase = str(config['DEFAULT']['Phase'])
+       accharger_limit = str(config['DEFAULT']['CurrentLimit'])
+
+       #send data to DBus
+       for phase in ['L1']:
+         pre = '/Ac/In/' + phase
+
+         if phase == accharger_phase:
+           power = meter_data['meters'][0]['power']
+           total = meter_data['meters'][0]['total']
+           current = power / 120
+
+           self._dbusservice[pre + '/CurrentLimit'] = accharger_limit
+           self._dbusservice[pre + '/I'] = current
+           self._dbusservice[pre + '/P'] = power
+           if power > 0:
+             self._dbusservice['/State'] = 3
+           else:
+             self._dbusservice['/State'] = 0
+
+         else:
+           self._dbusservice[pre + '/CurrentLimit'] = 0
+           self._dbusservice[pre + '/I'] = 0
+           self._dbusservice[pre + '/P'] = 0
+           self._dbusservice['/State'] = 0
+
+       self._dbusservice['/Ac/In/L1/P'] = self._dbusservice['/Ac/In/' + accharger_phase + '/P']
+
+       #logging
+       logging.debug("AC Charger Input (/Ac/In/L1/P): %s" % (self._dbusservice['/Ac/In/L1/P']))
+       logging.debug("---");
+
+       # increment UpdateIndex - to show that new data is available
+       index = self._dbusservice['/UpdateIndex'] + 1  # increment index
+       if index > 255:   # maximum value of the index
+         index = 0       # overflow from 255 to 0
+       self._dbusservice['/UpdateIndex'] = index
+
+       #update lastupdate vars
+       self._lastUpdate = time.time()
+    except Exception as e:
+       logging.critical('Error at %s', '_update', exc_info=e)
+
+    # return true, otherwise add_timeout will be removed from GObject - see docs http://library.isr.ist.utl.pt/docs/pygtk2reference/gobject-functions.html#function-gobject--timeout-add
+    return True
+
+  def _handlechangedvalue(self, path, value):
+    logging.debug("someone else updated %s to %s" % (path, value))
+    return True # accept the change
+
+
+
+def main():
+  #configure logging
+  logging.basicConfig(      format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                            datefmt='%Y-%m-%d %H:%M:%S',
+                            level=logging.INFO,
+                            handlers=[
+                                logging.FileHandler("%s/current.log" % (os.path.dirname(os.path.realpath(__file__)))),
+                                logging.StreamHandler()
+                            ])
+
+  try:
+      logging.info("Start");
+
+      from dbus.mainloop.glib import DBusGMainLoop
+      # Have a mainloop, so we can send/receive asynchronous calls to and from dbus
+      DBusGMainLoop(set_as_default=True)
+
+      #formatting
+      _kwh = lambda p, v: (str(round(v, 2)) + 'kWh')
+      _state = lambda p, v: (str(v))
+      _mode = lambda p, v: (str(v))
+      _a = lambda p, v: (str(round(v, 1)) + 'A')
+      _w = lambda p, v: (str(round(v, 1)) + 'W')
+      _l = lambda p, v: (str(round(v, 1)) + 'A')
+
+      #start our main-service
+      pvac_output = DbusShellyService(
+        servicename='com.victronenergy.charger',
+        paths={
+          '/Ac/In/L1/I': {'initial': 0, 'textformat': _a},
+          '/Ac/In/L1/P': {'initial': 0, 'textformat': _w},
+          '/Ac/In/CurrentLimit': {'initial': 0, 'textformat': _l},
+          '/State': {'initial': 0, 'textformat': _state},
+          '/Mode': {'initial': 4, 'textformat': _mode},
+        })
+
+      logging.info('Connected to dbus, and switching over to gobject.MainLoop() (= event based)')
+      mainloop = gobject.MainLoop()
+      mainloop.run()
+  except Exception as e:
+    logging.critical('Error at %s', 'main', exc_info=e)
+if __name__ == "__main__":
+  main()
